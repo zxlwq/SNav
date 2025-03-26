3fbe771a0f49c1c6191969b029e6412d5d406931
渡劫 <165335155+zxlwq@users.noreply.github.com>
2025-03-27 05:13:26 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -53,7 +53,7 @@
       </main>
       <div v-else id="loading">
         <img src="/icon/logo.png" alt="logo" class="logo" />
-        <span class="tip">开发中</span>
+        <span class="tip">科技-刘</span>
       </div>
     </Transition>
   </Provider>
