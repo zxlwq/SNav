68bf952aa4f0f17916930636ca9cab505460ebe5
渡劫 <165335155+zxlwq@users.noreply.github.com>
2025-03-31 16:57:42 +0800

    Update Cover.vue

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -77,9 +77,15 @@ const imgAnimationEnd = () => {
 // 图片显示失败
 const imgLoadError = () => {
   console.error("壁纸加载失败：", bgUrl.value);
-  $message.error("壁纸加载失败，已临时切换回默认");
-  bgUrl.value = `/background/bg${bgRandom}.jpg`;
+  $message.error("壁纸加载失败，已切换回自定义壁纸");
+
+  // 假设你有一个自定义壁纸 URL
+  const customFallbackUrl = 'https://images.zxl.cc.ua/blog/12.webp';
+
+  // 如果你有自定义壁纸，则使用它；否则，使用默认背景
+  bgUrl.value = customFallbackUrl || `/background/bg${bgRandom}.jpg`;
 };
+
 
 onMounted(() => {
   setBgUrl();
