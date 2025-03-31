22234e358a85ca2b8fff12bf6c975afc93f4055d
渡劫 <165335155+zxlwq@users.noreply.github.com>
2025-03-31 21:56:50 +0800

    Update AllBox.vue

--- src/components/AllFunc/AllBox.vue
+++ src/components/AllFunc/AllBox.vue
@@ -1,46 +1,19 @@
 <template>
   <n-tabs class="all-box" size="large" justify-content="space-evenly" animated>
-    <n-tab-pane
-      v-for="(tab, index) in tabs"
-      :key="tab.name"
-      class="no-padding height--full"
-      :name="tab.name"
-      :tab="tab.label"
-    >
-      <component :is="tab.component" v-if="tab.component" />
-      <span
-        v-else
-        contenteditable="true"
-        class="editable"
-        @input="updateContent($event, index)"
-      >{{ tab.content }}</span>
+    <n-tab-pane class="no-padding height--full" name="link" tab="捷径">
+      <ShortCut />
     </n-tab-pane>
+    <n-tab-pane name="note" tab="便签"> 即将完善 </n-tab-pane>
+    <n-tab-pane name="more" tab="待办"> 还能有啥呢 😢 </n-tab-pane>
   </n-tabs>
 </template>
 
 <script setup>
-import { ref } from "vue";
 import { NTabs, NTabPane } from "naive-ui";
 import ShortCut from "@/components/AllFunc/Box/ShortCut.vue";
-
-const tabs = ref([
-  { name: "link", label: "捷径", component: ShortCut },
-  { name: "note", label: "便签", content: "即将完善" },
-  { name: "more", label: "待办", content: "还能有啥呢 😢" }
-]);
-
-const updateContent = (event, index) => {
-  tabs.value[index].content = event.target.innerText;
-};
 </script>
-
 <style>
-.editable {
-  display: block;
-  min-height: 100px;
-  padding: 8px;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-  outline: none;
+.height--full {
+  height: 100%;
 }
 </style>
