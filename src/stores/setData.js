ddb24ebbed10e81b77e99403a9b2ddc5c52f2104
渡劫 <165335155+zxlwq@users.noreply.github.com>
2025-03-31 14:14:59 +0800

    Update setData.js

--- src/stores/setData.js
+++ src/stores/setData.js
@@ -7,8 +7,8 @@ const useSetDataStore = defineStore("setData", {
       themeType: "light",
       // 壁纸类别
       // 0 本地 / 1 必应 / 2 随机风景 / 3 随机动漫 / 4 自定义
-      backgroundType: 4,
-      backgroundCustom: "https://jpg.zxlwq.filegear-sg.me/PicGo/background3.jpg",
+      backgroundType: 0,
+      backgroundCustom: "https://images.zxl.cc.ua/blog/12.webp",
       // 壁纸遮罩
       showBackgroundGray: true,
       // 壁纸模糊
