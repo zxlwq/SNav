6231b93126a2b15dfd24b9b4b252e1a46cec5ece
渡劫 <165335155+zxlwq@users.noreply.github.com>
2025-03-31 14:55:13 +0800

    Update Cover.vue

--- src/components/Cover.vue
+++ src/components/Cover.vue
@@ -10,11 +10,23 @@
       @error.once="imgLoadError"
       @animationend="imgAnimationEnd"
     />
-    <Transition name="fade">
-      <div v-if="set.showBackgroundGray" class="gray" />
-    </Transition>
-  </div>
-</template>
+<Transition name="fade">
+   <div v-if="set.showBackgroundGray" class="background-overlay" />
+</Transition>
+<style scoped>
+.background-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url('https://images.zxl.cc.ua/blog/12.webp'); /* 替换成你的背景图片 */
+  background-size: cover;
+  background-position: center;
+  opacity: 0.5; /* 可调节透明度 */
+}
+</style>
+
 
 <script setup>
 import { ref, onMounted, onBeforeUnmount } from "vue";
