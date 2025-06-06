79c9723672eac57ee2e3787880dc6efdc7e5f5bf
科技-刘 <165335155+zxlwq@users.noreply.github.com>
2025-06-06 15:55:53 +0800

    Update setData.js

--- src/stores/setData.js
+++ src/stores/setData.js
@@ -8,7 +8,7 @@ const useSetDataStore = defineStore("setData", {
       // 壁纸类别
       // 0 本地 / 1 必应 / 2 随机风景 / 3 随机动漫 / 4 自定义
       backgroundType: 0,
-      backgroundCustom: "https://bloga.zxlwq.dpdns.org/jpg/favicon.webp",
+      backgroundCustom: "/background/favicon.jpg",
       // 壁纸遮罩
       showBackgroundGray: true,
       // 壁纸亮度（默认 100%）
